ad4c4bf585410a37375bdc9196151809c3333909
oak-hu <oakhu2005@gmail.com>
2025-01-20 12:27:38 +0000

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,8 +24,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Merriweather",
-        body: "Merriweather Sans",
+        header: "IBM Plex Mono",
+        body: "IBM Plex Mono",
         code: "IBM Plex Mono",
       },
       colors: {
