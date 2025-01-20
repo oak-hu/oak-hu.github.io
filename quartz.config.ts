ac0f9183803fed0adddb2b4c58329f2ed9e8682d
oak-hu <oakhu2005@gmail.com>
2025-01-20 12:59:06 +0000

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴🌸🎍 Oak Hu",
+    pageTitle: "🪴 Oak Hu",
     pageTitleSuffix: " | Oak Hu",
     enableSPA: true,
     enablePopovers: true,
