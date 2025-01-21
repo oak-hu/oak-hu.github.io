bbc7248efbf12a2515d703beaf66343bd4fcab37
oak-hu <oakhu2005@gmail.com>
2025-01-21 00:59:57 +0000

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -31,6 +31,7 @@ export const defaultContentPageLayout: PageLayout = {
   ],
   right: [
     Component.DesktopOnly(Component.TableOfContents()),
+    Component.MobileOnly(Component.Explorer()),
     Component.Backlinks(),
   ],
 }
