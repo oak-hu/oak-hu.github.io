abf6b73f319f75fdcfe8cd6647c677744ca1165f
oak-hu <oakhu2005@gmail.com>
2025-02-15 15:06:42 +0000

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Spectral SC",
-        body: "Palatino",
+        body: "Spectral",
         code: "IBM Plex Mono",
       },
       colors: {
